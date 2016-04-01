22e90b02331375e74ce2a668aa4f5f9f93c9a576
Anbarasi U <anmmagics@gmail.com>
2016-04-01 20:06:20 +0530

    Date range selection ability for Bore Reports

--- finances/src/main/java/com/anilicious/rigfinances/fragments/ReportBoreFragment.java
+++ finances/src/main/java/com/anilicious/rigfinances/fragments/ReportBoreFragment.java
@@ -1,11 +1,15 @@
 package com.anilicious.rigfinances.fragments;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.app.Fragment;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -35,6 +39,14 @@ public class ReportBoreFragment extends Fragment {
     TextView tvCasingPipeInHand;
     TextView tvEngineHrsStart;
     TextView tvEngineHrsEnd;
+    EditText etDateFrom;
+    EditText etDateTo;
+
+    private int currentYear;
+    private int currentMonth;
+    private int currentDate;
+
+    static final int DATE_DIALOG_ID = 1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -42,7 +54,8 @@ public class ReportBoreFragment extends Fragment {
 
         // UI Object References
         Button btnSubmit = (Button)view.findViewById(R.id.btn_submit);
-        tvDate = (TextView)view.findViewById(R.id.TextView2);
+        etDateFrom = (EditText)view.findViewById(R.id.date_from);
+        etDateTo = (EditText)view.findViewById(R.id.date_to);
         tvTotalDepth = (TextView)view.findViewById(R.id.TextView4);
         tvCastingDepth = (TextView)view.findViewById(R.id.TextView6);
         tvEngineHrsStart = (TextView)view.findViewById(R.id.TextView8);
@@ -54,7 +67,17 @@ public class ReportBoreFragment extends Fragment {
         tvDieselUsed = (TextView)view.findViewById(R.id.TextView26);
         tvDieselInHand = (TextView)view.findViewById(R.id.TextView28);
         tvCasingPipeInHand = (TextView)view.findViewById(R.id.TextView30);
-        retrieveDetailsFromDB();
+        final Button btnGenerate = (Button)view.findViewById(R.id.btn_generateBoreReport);
+
+        // Setup Date Picker
+        setupPickers();
+
+        btnGenerate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                retrieveDetailsFromDB();
+            }
+        });
 
         return view;
     }
@@ -66,9 +89,13 @@ public class ReportBoreFragment extends Fragment {
         Calendar calendar = Calendar.getInstance();
         int year = calendar.get(Calendar.YEAR);
 
-        String dateFroms = CommonUtils.FINANCIAL_YEAR_START + "/" + (year-1);
-        String dateTos = CommonUtils.FINANCIAL_YEAR_END + "/" + year;
-        String[] test=dateFroms.split("/");
+        //String dateFroms = CommonUtils.FINANCIAL_YEAR_START + "/" + (year-1);
+        //String dateTos = CommonUtils.FINANCIAL_YEAR_END + "/" + year;
+
+        int dateFrom = Integer.parseInt(CommonUtils.formatDateEntry(etDateFrom.getText().toString()));
+        int dateTo = Integer.parseInt(CommonUtils.formatDateEntry(etDateTo.getText().toString()));
+
+        /*String[] test=dateFroms.split("/");
         if(test[1].length()<=1)
         {
             test[1] = "0"+test[1];
@@ -89,10 +116,10 @@ public class ReportBoreFragment extends Fragment {
             test[0] = "0"+test[0];
         }
         date1 =(test[2]+test[1]+test[0]);
-        Integer dateTo= Integer.parseInt(date1);
+        Integer dateTo= Integer.parseInt(date1);*/
         reportsMap = reportsMapper.mapBoreDetails(dateFrom, dateTo);
 
-        tvDate.setText(dateFroms + " - " + dateTos);
+        //tvDate.setText(dateFroms + " - " + dateTos);
         tvTotalDepth.setText(reportsMap.get("total_depth").toString());
         tvCastingDepth.setText(reportsMap.get("casting_depth").toString());
         tvCommission.setText(reportsMap.get("commission").toString());
@@ -103,4 +130,43 @@ public class ReportBoreFragment extends Fragment {
         tvEngineHrsStart.setText(reportsMap.get("engine_hrs_start").toString());
         tvEngineHrsEnd.setText(reportsMap.get("engine_hrs_end").toString());
     }
+
+    private void setupPickers(){
+        /** Get the current date */
+        final Calendar cal = Calendar.getInstance();
+        currentYear = cal.get(Calendar.YEAR);
+        currentMonth = cal.get(Calendar.MONTH);
+        currentDate = cal.get(Calendar.DAY_OF_MONTH);
+
+        etDateFrom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(getActivity(), mDateFromSetListener, currentYear, currentMonth, currentDate).show();
+            }
+        });
+
+        etDateTo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(getActivity(), mDateToSetListener, currentYear, currentMonth, currentDate).show();
+            }
+        });
+    }
+
+    /* Callback received when the user "picks" a date in the dialog */
+    private DatePickerDialog.OnDateSetListener mDateFromSetListener =
+            new DatePickerDialog.OnDateSetListener() {
+                public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                    etDateFrom.setText(new StringBuilder().append(day).append("/")
+                            .append(month + 1).append("/").append(year).toString());
+                }
+            };
+
+    private DatePickerDialog.OnDateSetListener mDateToSetListener =
+            new DatePickerDialog.OnDateSetListener() {
+                public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                    etDateTo.setText(new StringBuilder().append(day).append("/")
+                            .append(month + 1).append("/").append(year).toString());
+                }
+            };
 }
